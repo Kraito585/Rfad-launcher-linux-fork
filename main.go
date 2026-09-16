c8f7d55918a12a743a3d1261d4a9f463e58a1787
Kraito585 <grigor4uk.pawel@gmail.com>
2026-09-16 23:14:18 +0900

    пмнрил

--- main.go
+++ main.go
@@ -19,8 +19,6 @@ var version = "dev"
 func init() {
     os.Setenv("WEBKIT_DISABLE_DMABUF_RENDERER", "1")
     os.Setenv("GDK_BACKEND", "x11")
-    
-    os.Setenv("WEBKIT_DISABLE_SANDBOX_THIS_IS_DANGEROUS", "1")
 }
 
 func main() {
